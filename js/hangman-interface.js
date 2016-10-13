6fb3d51dc5063c51a473e5fe9d56052a85501a0c
Dean Scelza and Ryan Apking <ryan@>
2016-10-13 11:49:34 -0700

    fixed rear end logic

--- js/hangman-interface.js
+++ js/hangman-interface.js
@@ -5,18 +5,24 @@ var Hangman = require ("./../js/hangman.js").hangmanModule;
 $(document).ready(function(){
   var hangbro = new Hangman();
   hangbro.getWord();
-  hangbro.splitWord();
-  $('#displayWord').text(hangbro.revealedWord);
+  // the next two commands are going to run too early
 
-  $('#letterGuess').submit(Function(event){
+
+
+
+  $('#letterGuess').submit(function(event){
     var theLetter = $('#actualLetter').val();
     hangbro.compareLetters(theLetter);
-    $('#displayWord').text(hangbro.revealedWord);
-    $('#lettersGuessed').text(hangbro.guessedLetters);
-    $('#remainingGuesses').text(hangbro.guessesRemaining);
-
-
-
+    $('#displayWord').text(hangbro.revealedWord.join(" "));
+    $('#lettersGuessed').text("LETTERS GUESSED:  " + hangbro.guessedLetters);
+    $('#remainingGuesses').text("GUESSES REMAINING:  " + hangbro.guessesRemaining);
+    $('img').attr("src", "img/" + hangbro.guessesRemaining + ".png");
+    if (hangbro.gameLost()){
+      $('#remainingGuesses').append("Sad trombone.  You have failed yourself.")
+    } else if (hangbro.gameWon()) {
+      $('#remainingGuesses').append("Congrats ~ You escaped the gallows ~ You must be smarter than a Rocket Surgeon.")
+    }
+    $('#actualLetter').val("");
     event.preventDefault();
   });
 
--- js/hangman.js
+++ js/hangman.js
@@ -8,21 +8,27 @@ function Hangman (){
 }
 
 Hangman.prototype.getWord = function(){
+  var currentHangman = this;
   $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1')
     .then(function(response) {
-      console.log(response);
+      currentHangman.word = response[0][0].toLowerCase();
+
+      currentHangman.splitWord();
+      $('#displayWord').text(currentHangman.revealedWord.join(' ' ));
   });
 };
 
 Hangman.prototype.splitWord = function(){
   //split API-generated word into wordLetters array and generate initial revealed word
+  var currentHangman = this;
   this.wordLetters = this.word.split("");
   this.wordLetters.forEach(function() {
-    this.revealedWord.push("_");
+    currentHangman.revealedWord.push("_");
   });
 };
 
 Hangman.prototype.compareLetters = function(guess){
+  guess = guess.toLowerCase();
   this.guessedLetters.push(guess);
   //check guess against wordLetters array, if guess wrong -> decrease guessesRemaining by 1
   if(this.wordLetters.includes(guess)){
@@ -37,19 +43,20 @@ Hangman.prototype.displayLetters = function(){
   // run through wordLetters array in a forEeach
   // If the letter has been guessed, push it to the array
   // if the letter has not been guessed, push '_' to the array
+  var currentHangman = this;
   this.revealedWord=[];
   this.wordLetters.forEach(function(letter){
-    if (this.guessedLetters.includes(letter)){
-      this.revealedWord.push(letter);
+    if (currentHangman.guessedLetters.includes(letter)){
+      currentHangman.revealedWord.push(letter);
     } else {
-      this.revealedWord.push("_");
+      currentHangman.revealedWord.push("_");
     }
   });
 };
 
 Hangman.prototype.gameLost = function(){
   //if guessesRemain=0 before all wordLetters revealed, then display, "Sorry you lost." message
-  if (this.guessesRemain === 0){
+  if (this.guessesRemaining === 0){
     return true;
   } else {
     return false;
